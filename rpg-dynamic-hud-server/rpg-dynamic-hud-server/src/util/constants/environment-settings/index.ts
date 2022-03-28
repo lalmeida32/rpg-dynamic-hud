7e9f614249b2ba9cd20647071348ba51e16d16aa
Lucas Almeida <lucasf.almeida.dev@gmail.com>
2022-03-28 20:25:23 -0300

    server:refact - Changing naming convention again (using 'interfaces')

--- rpg-dynamic-hud-server/rpg-dynamic-hud-server/src/util/constants/environment-settings/index.ts
+++ rpg-dynamic-hud-server/rpg-dynamic-hud-server/src/util/constants/environment-settings/index.ts
@@ -1,1 +1,12 @@
-export * from './implementation';
+import { IEnvironmentSettings } from './interfaces';
+
+export const envSettings: IEnvironmentSettings = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || '3000',
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || '3306',
+    username: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD || '',
+  },
+};
