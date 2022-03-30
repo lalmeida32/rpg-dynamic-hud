92157e396de5fe7b13568ea7ac39840a9582da22
Lucas Almeida <lucasf.almeida.dev@gmail.com>
2022-03-30 03:18:54 -0300

    server:fix - Fixing .env location bug

--- rpg-dynamic-hud-server/rpg-dynamic-hud-server/src/util/libs/server_settings_config/server_settings_config/serverSettingsConfig.ts
+++ rpg-dynamic-hud-server/rpg-dynamic-hud-server/src/util/libs/server_settings_config/server_settings_config/serverSettingsConfig.ts
@@ -24,10 +24,20 @@ for (const key in launchOptions.optional)
 if (serverSettingsModel.dotenv === undefined)
   serverSettingsModel.dotenv = defaultServerSettings.dotenv;
 
-if (serverSettingsModel.dotenv === 'cwd') dotenv.config();
-else if (serverSettingsModel.dotenv === 'bin')
-  dotenv.config({ path: path.resolve(process.argv0, '..', '.env') });
-else if (serverSettingsModel.dotenv !== 'none')
+if (serverSettingsModel.dotenv === 'cwd') {
+  const { error } = dotenv.config();
+  console.log(
+    `Loading dotenv in ${process.cwd()}: ${error ? 'error' : 'success'}`
+  );
+  if (error) throw Error(`Something happened when loading dotenv.`);
+} else if (serverSettingsModel.dotenv === 'bin') {
+  const dotenvPath = path.resolve(process.argv[0], '..', '.env');
+  const { error } = dotenv.config({ path: dotenvPath });
+  console.log(
+    `Loading dotenv in ${dotenvPath}: ${error ? 'error' : 'success'}`
+  );
+  if (error) throw Error(`Something happened when loading dotenv.`);
+} else if (serverSettingsModel.dotenv !== 'none')
   throw Error(`${serverSettingsModel.dotenv} is not a valid value for dotenv.`);
 
 /* Load environment variables or default */
