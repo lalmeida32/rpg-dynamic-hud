c43324d08866589c53f8d9967604b9df9f450202
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-05-02 18:17:23 -0300

    server/env:refact - Use keyof TServerSettingsModel as a type

--- server/src/util/server_settings_config/serverSettingsConfig.ts
+++ server/src/util/server_settings_config/serverSettingsConfig.ts
@@ -8,16 +8,18 @@ import { ISolvedArgs, argsSolver } from 'util/args_solver';
 import { TServerSettingsModel } from './TServerSettingsModel';
 import { settingsValidation } from './settingsValidation';
 
+type TKey = keyof TServerSettingsModel;
+
 const serverSettingsModel: TServerSettingsModel = {};
 for (const key in defaultServerSettings)
-  serverSettingsModel[key as keyof TServerSettingsModel] = undefined;
+  serverSettingsModel[key as TKey] = undefined;
 
 /* Load launch options */
 
 const launchOptions: ISolvedArgs = argsSolver(process.argv.splice(2));
 for (const key in launchOptions.optional)
   if (key in serverSettingsModel)
-    serverSettingsModel[key as keyof TServerSettingsModel] =
+    serverSettingsModel[key as TKey] =
       launchOptions.optional[key];
   else throw Error(`${key} launch option is not valid.`);
 
@@ -44,17 +46,17 @@ if (serverSettingsModel.dotenv === 'cwd') {
 
 /* Load environment variables or default */
 for (const key in serverSettingsModel) {
-  if (serverSettingsModel[key as keyof TServerSettingsModel] === undefined) {
-    serverSettingsModel[key as keyof TServerSettingsModel] =
+  if (serverSettingsModel[key as TKey] === undefined) {
+    serverSettingsModel[key as TKey] =
       process.env[key.toUpperCase()] ||
-      String(defaultServerSettings[key as keyof TServerSettingsModel]);
+      String(defaultServerSettings[key as TKey]);
   }
 }
 
 /* Save back to environment variables to be consistent */
 for (const key in serverSettingsModel) {
   process.env[key.toUpperCase()] =
-    serverSettingsModel[key as keyof TServerSettingsModel];
+    serverSettingsModel[key as TKey];
 }
 
 /* Validation and exporting */
