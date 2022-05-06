8209be0e93630d23a8623165bdc4d847255f1740
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-05-06 01:48:56 -0300

    client/feat:improve - Adding character sheet inputs and styling

--- client/src/app/app_room/CharacterSheet.tsx
+++ client/src/app/app_room/CharacterSheet.tsx
@@ -1,5 +1,42 @@
 import classes from './CharacterSheet.module.css';
+import inputClasses from '../../shared/components/input.module.css';
+import unknownCharacter from './unknown_character.gif';
 
 export const CharacterSheet = () => {
-  return <div className={classes.entry}></div>;
+  return (
+    <div className={classes.entry}>
+      <input type="text" className={inputClasses.textLikeInput} />
+      <img className={classes.characterMainImage} src={unknownCharacter} />
+      <div className={`${classes.statBarContainer} ${classes.marginBottomDiv}`}>
+        <input type="number" className={inputClasses.textLikeInput} />
+        <input type="number" className={inputClasses.textLikeInput} />
+      </div>
+      <div className={`${classes.statBarContainer} ${classes.marginBottomDiv}`}>
+        <input type="number" className={inputClasses.textLikeInput} />
+        <input type="number" className={inputClasses.textLikeInput} />
+      </div>
+      <div className={`${classes.statBarContainer} ${classes.marginBottomDiv}`}>
+        <input type="number" className={inputClasses.textLikeInput} />
+        <input type="number" className={inputClasses.textLikeInput} />
+      </div>
+      <div
+        className={`${classes.attributeContainer} ${classes.marginBottomDiv}`}
+      >
+        <input type="number" className={inputClasses.textLikeInput} />
+        <p className={inputClasses.textLikeInput}>Strength</p>
+      </div>
+      <div
+        className={`${classes.attributeContainer} ${classes.marginBottomDiv}`}
+      >
+        <input type="number" className={inputClasses.textLikeInput} />
+        <p className={inputClasses.textLikeInput}>Agility</p>
+      </div>
+      <div
+        className={`${classes.attributeContainer} ${classes.marginBottomDiv}`}
+      >
+        <input type="number" className={inputClasses.textLikeInput} />
+        <p className={inputClasses.textLikeInput}>Intelligence</p>
+      </div>
+    </div>
+  );
 };
