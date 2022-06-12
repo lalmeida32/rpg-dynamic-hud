c9942707c6959f1e847b803102fb535077bfb35a
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-06-12 19:05:18 -0300

    client/feat:new - Implementing dice chooser

--- client/src/app/app_game_room/AppGameRoom.tsx
+++ client/src/app/app_game_room/AppGameRoom.tsx
@@ -1,10 +1,16 @@
 import { CharacterSheet } from './character_sheet/CharacterSheet';
 import classes from './AppGameRoom.module.css';
+import { GameHud } from './game_hud/GameHud';
+import { DiceChooser } from './DiceChooser';
 
 export const AppGameRoom = () => {
   return (
     <div className={classes.app_game_room}>
       <CharacterSheet />
+      <div className={classes.game_hud_and_dice_chooser}>
+        <GameHud />
+        <DiceChooser />
+      </div>
     </div>
   );
 };
---
+++ client/src/app/app_game_room/game_hud/GameHud.tsx
@@ -0,0 +1,5 @@
+import classes from './GameHud.module.css';
+
+export const GameHud = () => {
+  return <div className={classes.game_hud}></div>;
+};
