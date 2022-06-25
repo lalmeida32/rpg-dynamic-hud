29a577bfce8955c4c42844afea542727db6a2d3c
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-06-25 15:29:29 -0300

    client/feat:remove - Removing private field

--- client/src/app/rooms_page/RoomCard.tsx
+++ client/src/app/rooms_page/RoomCard.tsx
@@ -1,18 +1,29 @@
 import classes from './RoomCard.module.css';
 import locked from 'shared/images/locked.svg';
+import unlocked from 'shared/images/unlocked.svg';
 import closed_book from 'shared/images/closed_book.svg';
 import { Button } from 'shared/components/Button';
 import { IRoomCardModel } from 'shared/models/IRoomCardModel';
+import { UserLoginContext } from 'shared/contexts/UserLogin';
+import { useContext } from 'react';
 
 interface IRoomCardProps {
   info: IRoomCardModel;
 }
 
 export const RoomCard: React.FC<IRoomCardProps> = props => {
+  const userLogin = useContext(UserLoginContext);
+
   return (
     <div className={classes.room_card}>
       <div className={classes.card_buttons}>
-        {props.info.private ? <img src={locked} /> : null}
+        {props.info.owner === userLogin.username ? (
+          props.info.opened ? (
+            <img src={unlocked} />
+          ) : (
+            <img src={locked} />
+          )
+        ) : null}
         <img src={closed_book} />
       </div>
       <p>{props.info.name}</p>
